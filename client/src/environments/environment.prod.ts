39c34fdf11357caf32f3109f9681f3c624793288
Simon <insomniamelodeath@gmail.com>
2026-06-06 00:14:56 +0000

    config: set native app URLs to Tailscale Funnel

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     apiBaseUrl: '/api',
     hubUrl: '/hubs/ghostlist',
-    nativeApiBaseUrl: 'http://192.168.10.53/api',
-    nativeHubUrl: 'http://192.168.10.53/hubs/ghostlist',
+    nativeApiBaseUrl: 'https://red-queen-zulu.tail383b32.ts.net/api',
+    nativeHubUrl: 'https://red-queen-zulu.tail383b32.ts.net/hubs/ghostlist',
 };
